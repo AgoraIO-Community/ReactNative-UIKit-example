d92d42b010513ae0e793a9544bb7cce3c41dc02f
technophilic <svineth.face@gmail.com>
2020-11-16 11:24:31 +0530

    init example

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import AgoraUIKit from 'agora-rn-uikit';
+
+const App = () => {
+  const [videoCall, setVideoCall] = useState(true);
+  const rtcProps = {
+    appId: '<-----App ID here----->',
+    channel: 'test',
+  };
+  const callbacks = {
+    EndCall: () => setVideoCall(false),
+  };
+  return videoCall ? (
+    <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+  ) : (
+    <></>
+  );
+};
+
+export default App;
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  uikitExample
+//
+//  Created by APPLE on 16/11/20.
+//
+
+import Foundation
